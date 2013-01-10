c486a77b4a681566d1c59598616ca58a97ce8c93
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2013-01-10 12:52:29 +0200

    Make this as an installable package sphinxcontrib.tikz

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+long_desc = '''
+This package contains the tikz Sphinx extension, which enables the use
+of the PGF/TikZ LaTeX package to draw nice pictures.
+'''
+
+requires = ['Sphinx>=0.6']
+
+setup(
+    name='sphinxcontrib-tikz',
+    version='0.4',
+    url='http://bitbucket.org/birkenfeld/sphinx-contrib',
+    download_url='http://pypi.python.org/pypi/sphinxcontrib-tikz',
+    license='BSD',
+    author='Christoph Reller',
+    author_email='creller@ee.ethz.ch',
+    maintainer='Jaakko Luttinen',
+    maintainer_email='jaakko.luttinen@iki.fi',
+    description='Sphinx "tikz" extension',
+    long_description=long_desc,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+    use_2to3 = True,
+)
